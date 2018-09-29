8f6c34d0e8192af7a279810b73d8269cab371ac2
Carlos <carlos.abril.herrero@gmail.com>
2018-09-29 16:51:10 +0200

    fixed convert methods

--- src/Model/State.java
+++ src/Model/State.java
@@ -47,7 +47,7 @@ public class State {
         return board2D;
     }
 
-    public void convertBoard1D(int[] board2D){ //convert back to 1D array
+    public void convertBoard1D(int[][] board2D){ //convert back to 1D array
         int counter = 0;
         for(int i = 0; i<size; i++) {
             for (int j = 0; j < size; j++) {
