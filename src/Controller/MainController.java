27c6d66012dfa568e2fa38fe90f55438c754f5a9
vincevandriel <vincevandriel@gmail.com>
2018-10-11 16:11:56 +0200

    Fixed Error

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -8,10 +8,11 @@ import java.awt.*;
 public class MainController {
     private static Dimension DEFAULT_BOARD_SIZE = new Dimension(250,250);
     public void start(Window window){
+        int player1 = 0; int player2 = 0;
         Board board = new Board(DEFAULT_BOARD_SIZE);
         window.displayBoard(board);
         window.setVisible(true);
         BoardController boardController = new BoardController();
-        boardController.start(board);
+        boardController.start(board,player1,player2);
     }
 }
