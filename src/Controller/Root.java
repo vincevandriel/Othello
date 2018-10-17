ef22cb80f14897cc87dcb2cbaac27497ef20ffcb
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2018-10-17 14:08:45 +0200

    Made a method to add children (not sure if this works...)

--- src/Controller/Root.java
+++ src/Controller/Root.java
@@ -11,11 +11,36 @@ public class Root {
         board = this.board;
         tile = this.tile;
     }
+    /*
     public void addChildren(List<Node> children){ //checked
             for(int i = 0; i<children.size(); i++){
                 children.get(i).setParent(this);
                 this.children.add(children.get(i));
             }
+    }
+    */
+
+    public void addChildren(int[][] board, int tile) {
+        Rules rules = new Rules();
+        int[][] tempBoard = rules.checkMoves(board, tile, 0, 0);
+
+        tile = reverseTile(tile);
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(tempBoard[i][j] == 3) {
+                    children.add(new Node(null, i, j, tile));
+                }
+            }
+        }
+    }
+
+    public int reverseTile(int tile) {
+        if(tile == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     public int[][] retrieveBoard(int[][] currentBoard, Node start) {
