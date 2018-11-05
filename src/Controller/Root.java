c1d6002a3b0bcae2f59cf897cac1e181901e4aba
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2018-11-05 06:58:45 +0100

    Fixed a bug
    Tree is technically done but no bugtesting has been done

--- src/Controller/Root.java
+++ src/Controller/Root.java
@@ -1,3 +1,4 @@
+import Controller.Node;
 import Model.Rules;
 
 import java.util.ArrayList;
@@ -85,7 +86,6 @@ public class Root {
                 }
             }
 
-            Rules rules = new Rules();
             int[][] tempBoard = new int[board.length][board[0].length];
             for(int i = 0; i < board.length; i++) {
                 for(int j = 0; j < board[0].length; j++) {
