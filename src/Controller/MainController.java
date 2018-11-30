b178a76b63235b061923c316ae61a0f9084ae2f3
Carlos <carlos.abril.herrero@gmail.com>
2018-11-30 16:30:11 +0100

    updated classes. Alpha-Beta now paired with player

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -9,8 +9,8 @@ import java.awt.*;
 public class MainController {
     private static Dimension DEFAULT_BOARD_SIZE = new Dimension(250,250);
     public void start(Window window){
-        Human P1 = new Human(1);
-        AlphaBeta P2 = new AlphaBeta();
+        Player P1 = new Human(1);
+        Player P2 = new AlphaBeta();
         Board board = new Board(DEFAULT_BOARD_SIZE);
         window.displayBoard(board);
         window.setVisible(true);
