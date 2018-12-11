a1996d0829d8775ad87e1f4ab391b380ee067b15
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2018-12-11 09:02:46 +0100

    Added a Mobility-based new Evaluation Function

---
+++ src/Model/MobilityEval.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class MobilityEval implements EvalFunction{
+    private static Rules rules;
+
+    @Override
+    public int eval(int[][] board, int tile) {
+        if(rules == null) {
+            rules = new Rules();
+        }
+        int amount = 0;
+        int[][] temp = rules.checkMoves(board, tile);
+        for(int i = 0; i < temp.length; i++) {
+            for(int j = 0; j < temp[0].length; j++) {
+                if(temp[i][j] == 3) {
+                    amount++;
+                }
+            }
+        }
+        rules.clear3s(temp);
+        return amount;
+    }
+}
