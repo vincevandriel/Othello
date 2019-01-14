d5faf1592c1bbcc1dd919a108930957f5f12d051
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2019-01-14 13:22:09 +0100

    Merged both MobilityEval Functions and TileCounter Functions together

--- src/Model/MobilityEval.java
+++ src/Model/MobilityEval.java
@@ -1,26 +1,79 @@
 package Model;
 
+//This evaluation function calculates the score based on amount of moves that can be done for both players
 public class MobilityEval implements EvalFunction{
-
-    //This evaluation function calculates the score based on amount of moves that can be done later on in the game
 
     private static Rules rules;
     private double evalWeight;
 
+    //This is how much is added to the score for the moves of the player
+    private int thisPlayerValue;
+    //This is how much is subtracted from the score for the moves of the opponent
+    private int opponentPlayerValue;
+
     public MobilityEval() {
+        this.thisPlayerValue = 1;
+        this.opponentPlayerValue = 0;
         rules = new Rules();
         this.evalWeight = 0.01;
     }
 
+    //custom evalWeight
     public MobilityEval(double evalWeight) {
+        this.thisPlayerValue = 1;
+        this.opponentPlayerValue = 0;
+        rules = new Rules();
+        this.evalWeight = evalWeight;
+    }
+
+    //custom tileValues
+    public MobilityEval(int thisTileValue, int opponentTileValue) {
+        this.thisPlayerValue = thisTileValue;
+        this.opponentPlayerValue = opponentTileValue;
+        rules = new Rules();
+        this.evalWeight = 0.01;
+    }
+
+    //custom tileValues AND evalWeight
+    public MobilityEval(int thisTileValue, int opponentTileValue, double evalWeight) {
+        this.thisPlayerValue = thisTileValue;
+        this.opponentPlayerValue = opponentTileValue;
         rules = new Rules();
         this.evalWeight = evalWeight;
     }
 
     @Override
     public int eval(int[][] board, int tile) {
-        int amount = 0;
+        int score = 0;
+        if(thisPlayerValue != 0) {
+            score += thisPlayerValue*checkMovesAmount(board, tile);
+        }
+        if(opponentPlayerValue != 0) {
+            score -= opponentPlayerValue*checkMovesAmount(board, swaptile(tile));
+        }
+        return score;
+    }
+
+    @Override
+    public double getEvalWeight(){
+        return evalWeight;
+    }
+
+    public int swaptile(int tile) {
+        if(tile == 1){
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    //Returns the amount of moves for the given player/tile
+    public int checkMovesAmount(int [][] board, int tile) {
+        //Make a new, temporary board to see the amount of moves
         int[][] temp = rules.checkMoves(board, tile);
+        int amount = 0;
+
+        //check the amount of moves available for this player
         for(int i = 0; i < temp.length; i++) {
             for(int j = 0; j < temp[0].length; j++) {
                 if(temp[i][j] == 3) {
@@ -28,12 +81,9 @@ public class MobilityEval implements EvalFunction{
                 }
             }
         }
+
+        //clear 3's and return
         rules.clear3s(temp);
         return amount;
-    }
-
-    @Override
-    public double getEvalWeight(){
-        return evalWeight;
     }
 }
--- src/Model/TileCounter.java
+++ src/Model/TileCounter.java
@@ -1,40 +1,68 @@
 package Model;
 
+//This evaluation function calculates the score based on the amount of tiles for both players
 public class TileCounter implements EvalFunction{
-
-    //This evaluation function uses as score the amount of tiles.
 
     private double evalWeight;
 
-    public TileCounter(){
-        evalWeight = 0.01;
+    //This is how much is added to the score for our own tiles.
+    private int thisPlayerValue;
+    //This is how much is subtracted from the score for the tiles of the opponent.
+    private int opponentPlayerValue;
+
+    public TileCounter() {
+        this.thisPlayerValue = 1;
+        this.opponentPlayerValue = 0;
+        this.evalWeight = 0.01;
     }
 
-    public TileCounter(double evalWeight){
+    //custom evalWeight
+    public TileCounter(double evalWeight) {
+        this.thisPlayerValue = 1;
+        this.opponentPlayerValue = 0;
+        this.evalWeight = evalWeight;
+    }
+
+    //custom tileValues
+    public TileCounter(int thisTileValue, int opponentTileValue) {
+        this.thisPlayerValue = thisTileValue;
+        this.opponentPlayerValue = opponentTileValue;
+        this.evalWeight = 0.01;
+    }
+
+    //custom tileValues and evalWeight
+    public TileCounter(int thisTileValue, int opponentTileValue, double evalWeight) {
+        this.thisPlayerValue = thisTileValue;
+        this.opponentPlayerValue = opponentTileValue;
         this.evalWeight = evalWeight;
     }
 
     @Override
-    public int eval(int[][] board, int colour) {
+    public int eval(int[][] board, int tile) {
         int score = 0;
 
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[0].length; j++) {
-                if(board[i][j] == colour) {
-                    score++;
+                if(thisPlayerValue != 0 && board[i][j] == tile) {
+                    score += thisPlayerValue;
+                } else if(opponentPlayerValue != 0 && board[i][j] == getOpponentColour(tile)) {
+                    score -= opponentPlayerValue;
                 }
             }
         }
-
         return score;
     }
-    public int getMax(int[][] board) {
-        return board.length*board.length;
-    }
-
 
     @Override
     public double getEvalWeight(){
         return evalWeight;
+    }
+
+    public int getOpponentColour(int tile) {
+        if(tile == 1){
+            return 2;
+        } else {
+            return 1;
+        }
     }
 }
